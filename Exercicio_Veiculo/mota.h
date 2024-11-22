d13c17a566aedee950f1baf6e3990699192a7ed2
Gonçalo Branco <113313553+goncalobranco299@users.noreply.github.com>
2024-11-22 10:22:37 -0100

    mota.h

--- Exercicio_Veiculo/mota.h
+++ Exercicio_Veiculo/mota.h
@@ -7,8 +7,13 @@ class Mota : public Veiculo{ // Define a classe "Mota" que herda da classe base 
     private:
         string tipoGuiadao; // Declara uma variável privada para armazenar o tipo de guiador da mota
     public:
+        // Construtor da classe "Mota".
+        // Ele inicializa os atributos do veículo utilizando o construtor da classe base "Veiculo",
+        // e também inicializa o atributo específico "tipoGuiadao".
         Mota(string nome, string rodas,string tipoGuiadao, int velocidadeMaxima, int velocidadeAtual)
             : Veiculo(nome, rodas, velocidadeMaxima, velocidadeAtual),tipoGuiadao(tipoGuiadao){}
+        // Declaração de um método público chamado "menuMota".
+        // Este método provavelmente será usado para exibir opções ou funcionalidades específicas da moto.
     void menuMota();
 };
 
